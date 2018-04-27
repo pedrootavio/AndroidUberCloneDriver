2b499347b1aa717188b1584b1271cac36e333d02
Pedro Neto <pedrootavio.neto@gmail.com>
2018-04-27 10:36:57 -0300

    Android Uber Clone - Part 1 : Sign Up / Sign In

---
+++ app/src/main/java/uber/com/br/androiduberclone/Model/User.java
@@ -0,0 +1,47 @@
+package uber.com.br.androiduberclone.Model;
+
+public class User {
+    private String email, password, name, phone;
+
+    public User() {
+    }
+
+    public User(String email, String password, String name, String phone) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
